eae9a245c48f0669bb5f41cb41830538d7d02ca1
Austin Jackson <themacphage@gmail.com>
2022-01-01 16:17:11 -0600

    wtf is going on

--- src/_firebaseHelper.ts
+++ src/_firebaseHelper.ts
@@ -1,5 +1,7 @@
 import * as admin from 'firebase-admin'
 
+console.log('what is: ',process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT)
+
 const serviceAccount = process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT ? JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT) : require('../.secret/firebase-adminsdk.json');
 
 export const firebase = !admin.apps.length ? admin.initializeApp({
