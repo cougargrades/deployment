7df93945b60937e04ea56a7d4775adb17beb5742
Austin Jackson <themacphage@gmail.com>
2022-01-01 16:24:16 -0600

    try this

--- src/_firebaseHelper.ts
+++ src/_firebaseHelper.ts
@@ -1,8 +1,8 @@
 import * as admin from 'firebase-admin'
 
-console.log('what is: ',process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT)
+console.log('what is: ',process.env.FIREBASE_SERVICE_ACCOUNT)
 
-const serviceAccount = process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT ? JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT) : require('../.secret/firebase-adminsdk.json');
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT) : require('../.secret/firebase-adminsdk.json');
 
 export const firebase = !admin.apps.length ? admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
