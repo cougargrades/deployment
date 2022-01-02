85f4a1bb6b79dd2f1f51b34af3d1de2cd8f52f91
Austin Jackson <themacphage@gmail.com>
2022-01-01 18:04:46 -0600

    removed more crap

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ async function run(argv) {
   const cli = build()
     .brand('deployment')
     .src(__dirname)
-    .plugins('./node_modules', { matching: 'deployment-*', hidden: true })
+    //.plugins('./node_modules', { matching: 'deployment-*', hidden: true })
     .help() // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
     .create()
