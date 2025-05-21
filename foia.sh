0efd6acc0e870b0b29c748c5422c26ba1cc49358
Austin Jackson <austinjckson@gmail.com>
2025-05-21 12:04:16 -0500

    ffs

--- foia.sh
+++
@@ -1,9 +0,0 @@
-
-az communication email send \
-            --connection-string $ACS_CONNECTION_STRING \
-            --sender "DoNotReply@cougargrades.io" \
-            --to $MAIL_TO \
-            --cc $MAIL_CC \
-            --reply-to $MAIL_REPLY_TO \
-            --subject $MAIL_SUBJECT \
-            --text $MAIL_PLAINTEXT_BODY
